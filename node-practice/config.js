6a70eca235c7b6f15ce463fe6563ea765335dc78
sooarin <kww216@naver.com>
2021-04-30 19:00:45 +0900

    first commit

---
+++ node-practice/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI : 'mongodb+srv://soarin:kw19960515@cluster0.ccxvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ node-practice/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./config');
+const postsRoutes = require('./routes/api/posts_controller')
+const app = express();
+app.use(express.json());
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+})
+    .then(() => console.log('MongoDB connected!'))
+    .catch(err => console.log(err));
+
+app.use('/api/posts', postsRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running at port ${PORT}'));
